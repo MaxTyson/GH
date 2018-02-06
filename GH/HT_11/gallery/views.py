0e0bc71fa8faf65a16f20c55c81eee9e2ddea192
MaxTyson <maxtyson88@gmail.com>
2018-02-07 00:03:44 +0200

    made simple photo gallery

--- GH/HT_11/gallery/views.py
+++ GH/HT_11/gallery/views.py
@@ -25,35 +25,17 @@ class ResultsView(generic.DetailView):
 
 
 def add_like(request, photo_id):
-    name = get_object_or_404(Photo, pk=photo_id)
+    photo = get_object_or_404(Photo, pk=photo_id)
     try:
-        selected_like = name.like_set.get(pk=request.POST['like'])
+        selected_like = photo.like_set.get(pk=request.POST['like'])
     except (KeyError, Like.DoesNotExist):
         # Redisplay the question add like form.
         return render(request, 'gallery/detail.html', {
-            'name': name,
+            'photo': photo,
             'error_message': "You didn't add a like.",
         })
     else:
         selected_like.likes += 1
         selected_like.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('gallery:results', args=(photo.id,)))
 
-
-# def index(request):
-
-#     latest_photos_list = Photo.objects.order_by('-pub_date')[:5]
-#     context = {'latest_photos_list': latest_photos_list}
-#     return render(request, 'gallery/index.html',(context))
-# def detail(request, photo_id):
-
-#     photo = get_object_or_404(Photo, pk=photo_id)
-#     return render(request, 'gallery/detail.html', {'photo': photo})
-# def results(request, photo_id):
-
-
-#     photo = get_object_or_404(Photo, pk=photo_id)
-#     return render(request, 'gallery/results.html', {'photo': photo})
+        return HttpResponseRedirect(reverse('gallery:results', args=(photo_id,)))
---
+++ GH/HT_11/readme.txt
@@ -0,0 +1,5 @@
+Simple photo gallery by Django.
+Functional:
+    - photos add from admin panel;
+    - can add likes to photo on the page.
+Warning! Project made for learning and may has errors.
